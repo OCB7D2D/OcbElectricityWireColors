71991a49bef3e93a87f87ead3ee42224e9bf21b3
Marcel Greter <marcel.greter@ocbnet.ch>
2021-10-29 17:04:57 +0200

    Fix initial coloring for dedicated server mode

--- Harmony/ocbElectricityWireColors.cs
+++ Harmony/ocbElectricityWireColors.cs
@@ -2,6 +2,7 @@ using DMT;
 using HarmonyLib;
 using UnityEngine;
 using System.Reflection;
+using System.Collections;
 
 public class OcbElectricityWireColors
 {
@@ -18,11 +19,11 @@ public class OcbElectricityWireColors
     }
     
     // Helper function to update pulse color if necessary
-    public static void UpdatePulseColor(IWireNode wire, Color col)
+    public static void UpdatePulseColor(IWireNode wire, Color col, bool force = false)
     {
         if (wire is FastWireNode fast)
         {
-            if (fast.pulseColor != col)
+            if (fast.pulseColor != col || true)
             {
                 fast.SetPulseColor(col);
                 fast.TogglePulse(WireManager.Instance.ShowPulse);
@@ -30,12 +31,46 @@ public class OcbElectricityWireColors
         }
         else if (wire is WireNode node)
         {
-            if (node.pulseColor != col)
+            if (node.pulseColor != col || true)
             {
                 node.SetPulseColor(col);
                 node.TogglePulse(WireManager.Instance.ShowPulse);
             }
         }
+    }
+
+    public static void UpdateWireColor(TileEntityPowered __instance)
+    {
+        // This check is copied from the original function
+        // if (!((UnityEngine.Object)__instance.BlockTransform != (UnityEngine.Object)null))
+        //     return;
+        // Get the power state for this power tile entity
+        bool isPowered = __instance.IsPowered;
+        // Default pulse color is yellow or gray
+        Color pulseColor = isPowered ? Color.yellow : Color.gray;
+        // Check if we have a parent wire of the same type
+        if (__instance.ParentWire != null && __instance.isParentSameType)
+        {
+            if (__instance.PowerItemType == PowerItem.PowerItemTypes.TripWireRelay)
+            {
+                pulseColor = isPowered ? Color.magenta : Color.cyan;
+            }
+            else if (__instance.PowerItemType == PowerItem.PowerItemTypes.ElectricWireRelay)
+            {
+                pulseColor = isPowered ? Color.red : Color.blue;
+            }
+            else {
+                pulseColor = Color.green;
+            }
+        }
+        // Update pulse color (if needed/changed)
+        UpdatePulseColor(__instance.ParentWire, pulseColor, true);
+    }
+
+    public static IEnumerator updateWiresLater(TileEntityPowered __instance)
+    {
+        yield return (object) new WaitForSeconds(0.5f);
+        UpdateWireColor(__instance);
     }
 
     // Do some additional work after DrawWires was executed
@@ -47,59 +82,70 @@ public class OcbElectricityWireColors
         static void Postfix(TileEntityPowered __instance)
         {
             // Check that counts match up (should always be the case, but be cautious)
-            if (__instance.wireDataList.Count != __instance.currentWireNodes.Count) return;
-            for (int i = 0; i < __instance.wireDataList.Count; ++i)
+            // if (__instance.wireDataList.Count != __instance.currentWireNodes.Count) {
+            //     Log.Out("TileEntityPowered not correctly initialized?");
+            // }
+            // ToDo: Check why this can be different at all after `DrawWires`
+            // Note: seems to work none the less (so don't really care too much)
+            for (int i = 0; i < __instance.currentWireNodes.Count; ++i)
             {
-                Vector3i data = __instance.wireDataList[i];
                 IWireNode wire = __instance.currentWireNodes[i];
+                Vector3i  data = new Vector3i(wire.GetEndPosition());
                 if (GameManager.Instance.World.GetChunkFromWorldPos(data) is Chunk chunk)
                 {
                     TileEntityPowered tileEntity = GameManager.Instance.World
                         .GetTileEntity(chunk.ClrIdx, data) as TileEntityPowered;
+                    if (tileEntity == null) continue;
                     if (wire is FastWireNode fast)
                     {
                         tileEntity.isParentSameType = fast.pulseColor != Color.yellow
-                                                      && fast.pulseColor != Color.gray;
+                                                        && fast.pulseColor != Color.gray;
                     }
                     else if (wire is WireNode node)
                     {
                         tileEntity.isParentSameType = node.pulseColor != Color.yellow
-                                                      && node.pulseColor != Color.gray;
+                                                        && node.pulseColor != Color.gray;
                     }
                     tileEntity.ParentWire = wire;
                 }
             }
+            GameManager.Instance.StartCoroutine(updateWiresLater(__instance));
+            UpdateWireColor(__instance);
         }
     }
 
-    // Update pulseColor on each tick (maybe add another delay?)
+    // Update pulseColor on each tick (MP mode)
     [HarmonyPatch(typeof(TileEntityPowered))]
-    [HarmonyPatch("UpdateTick")]
-    public class TileEntityPowerSource_UpdateTick
+    [HarmonyPatch("read")]
+    public class TileEntityPoweredBlock_read
     {
-        static void Postfix(TileEntityPowered __instance)
+        static void Postfix(TileEntityPoweredBlock __instance)
         {
-            // This check is copied from the original function
-            if (!((UnityEngine.Object)__instance.BlockTransform != (UnityEngine.Object)null))
-                return;
-            // Get the power state for this power tile entity
-            bool isPowered = __instance.PowerItem != null && __instance.PowerItem.IsPowered;
-            // Default pulse color is yellow or gray
-            Color pulseColor = isPowered ? Color.yellow : Color.gray;
-            // Check if we have a parent wire of the same type
-            if (__instance.ParentWire != null && __instance.isParentSameType)
-            {
-                if (__instance.PowerItemType == PowerItem.PowerItemTypes.TripWireRelay)
-                {
-                    pulseColor = isPowered ? Color.magenta : Color.cyan;
-                }
-                else if (__instance.PowerItemType == PowerItem.PowerItemTypes.ElectricWireRelay)
-                {
-                    pulseColor = isPowered ? Color.red : Color.blue;
-                }
-            }
-            // Update pulse color (if needed/changed)
-            UpdatePulseColor(__instance.ParentWire, pulseColor);
+            UpdateWireColor(__instance);
+        }
+    }
+
+    // Update pulseColor on each tick (MP mode)
+    [HarmonyPatch(typeof(PowerConsumer))]
+    [HarmonyPatch("HandlePowerUpdate")]
+    public class PowerConsumer_HandlePowerUpdate
+    {
+        static void Postfix(PowerConsumer __instance)
+        {
+            if (__instance.TileEntity != null)
+                UpdateWireColor(__instance.TileEntity);
+        }
+    }
+
+    // Update pulseColor on each tick (MP mode)
+    [HarmonyPatch(typeof(PowerConsumer))]
+    [HarmonyPatch("IsPoweredChanged")]
+    public class PowerConsumer_IsPoweredChanged
+    {
+        static void Postfix(PowerConsumer __instance)
+        {
+            if (__instance.TileEntity != null)
+                UpdateWireColor(__instance.TileEntity);
         }
     }
 
